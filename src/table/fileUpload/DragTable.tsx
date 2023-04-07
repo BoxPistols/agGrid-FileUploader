97c53bab8beb8505001dc1ca0417495c6207db6c
A_ito <ito.atsu.mail@gmail.com>
2023-04-07 21:34:54 +0000

    Update 3 files

--- src/table/fileUpload/DragTable.tsx
+++ src/table/fileUpload/DragTable.tsx
@@ -1,23 +1,21 @@
-import React, { useRef, useState, ChangeEvent } from "react";
-import { Box, Input, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Button } from "@mui/material";
 import { AgGridReact } from "ag-grid-react";
 import { ColDef } from "ag-grid-community";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-balham.css";
 
-const DragAndDropUploader = () => {
+const DragTable = () => {
   const [, setFile] = useState<File | null>(null);
   const [columnDefs, setColumnDefs] = useState<ColDef[]>([]);
   const [rowData, setRowData] = useState<{ [key: string]: string }[]>([]);
 
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (files) {
-      setFile(files[0]);
-    }
-  };
+  //   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //     const files = event.target.files;
+  //     if (files) {
+  //       setFile(files[0]);
+  //     }
+  //   };
 
   const parseCsvData = (csvData: string) => {
     const [headers, ...rows] = csvData.trim().split("\n");
@@ -69,58 +67,38 @@ const DragAndDropUploader = () => {
     }
   };
 
+  // AgGrid
+  const customNoRowsMessage =
+    "ここにファイルをドロップして添付アップロードしてください";
+
   return (
     <div>
-      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-        <div
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-          style={{
-            border: "2px dashed #ccc",
-            padding: 20,
-            textAlign: "center",
-            width: "auto",
-            height: 60,
-            marginBottom: 16,
-          }}
-        >
-          <Typography sx={{ color: "dimgray" }}>CSVファイル</Typography>
-          <Input
-            ref={fileInputRef}
-            type="file"
-            inputProps={{ accept: ".csv" }}
-            onChange={handleFileChange}
-            style={{ display: "none" }}
-          />
-          <Button
-            onClick={() => fileInputRef.current?.click()}
-            variant="text"
-            color="primary"
-          >
-            ここにファイルをドロップしてください
-          </Button>
-        </div>
-        {rowData.length > 0 && (
-          <Button onClick={handleReset} color="secondary" variant="outlined">
-            リセット
-          </Button>
-        )}
-      </Box>
       <div
         className="ag-theme-alpine"
-        style={{ height: 200, width: "100%" }}
-        // onDragOver={handleDragOver}
-        // onDrop={handleDrop}
+        style={{ height: 180, width: "100%" }}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
       >
-        <AgGridReact columnDefs={columnDefs} rowData={rowData} />
+        <AgGridReact
+          columnDefs={columnDefs}
+          rowData={rowData}
+          localeText={{
+            noRowsToShow: customNoRowsMessage,
+          }}
+        />
       </div>
-      {/* {rowData.length > 0 && (
-        <Button onClick={handleReset} color="secondary" variant="contained">
+      {rowData.length > 0 && (
+        <Button
+          onClick={handleReset}
+          color="secondary"
+          variant="outlined"
+          sx={{ mt: 1 }}
+        >
           リセット
         </Button>
-      )}{" "} */}
+      )}{" "}
     </div>
   );
 };
 
-export default DragAndDropUploader;
+export default DragTable;
